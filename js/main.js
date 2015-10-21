76fa806f68feae51cb6d122ec2e0f8b3821f071b
Eric Frommelt <ericfrommelt@gmail.com>
2015-10-21 09:22:44 -0400

    added initial client project pages

--- js/main.js
+++ js/main.js
@@ -28,6 +28,81 @@ var pages = {
           {
             "title": "Oculus Rift",
             "url": "http://sites:8888/ericfrommelt2015/_site/work/oculus-rift/"
+          },
+
+          {
+            "title": "Visions for Space",
+            "url": "http://sites:8888/ericfrommelt2015/_site/work/usc-visions-for-space/"
+          },
+
+          {
+            "title": "Shotlink",
+            "url": "http://sites:8888/ericfrommelt2015/_site/work/numbers-shotlink/"
+          },
+
+          {
+            "title": "Lakers Numbers",
+            "url": "http://sites:8888/ericfrommelt2015/_site/work/numbers-lakers/"
+          },
+
+          {
+            "title": "Technology Policy",
+            "url": "http://sites:8888/ericfrommelt2015/_site/work/technology-policy/"
+          },
+
+          {
+            "title": "Creative Computers",
+            "url": "http://sites:8888/ericfrommelt2015/_site/work/creative-computers/"
+          },
+
+          {
+            "title": "Urban Mobility",
+            "url": "http://sites:8888/ericfrommelt2015/_site/work/urban-mobility/"
+          },
+
+          {
+            "title": "Get Cryphy",
+            "url": "http://sites:8888/ericfrommelt2015/_site/work/get-cryphy/"
+          },
+
+          {
+            "title": "New Car",
+            "url": "http://sites:8888/ericfrommelt2015/_site/work/new-car/"
+          },
+
+          {
+            "title": "IBM Pure Data",
+            "url": "http://sites:8888/ericfrommelt2015/_site/work/ibm-pure-data/"
+          },
+
+          {
+            "title": "Quant Junkies",
+            "url": "http://sites:8888/ericfrommelt2015/_site/work/quant-junkies/"
+          },
+
+          {
+            "title": "Weird Tales: Vampires",
+            "url": "http://sites:8888/ericfrommelt2015/_site/work/weird-tales/"
+          },
+
+          {
+            "title": "EnerG2",
+            "url": "http://sites:8888/ericfrommelt2015/_site/work/energ2/"
+          },
+
+          {
+            "title": "Styleshifters",
+            "url": "http://sites:8888/ericfrommelt2015/_site/work/styleshifters/"
+          },
+
+          {
+            "title": "Impact of Media",
+            "url": "http://sites:8888/ericfrommelt2015/_site/work/impact-of-media/"
+          },
+
+          {
+            "title": "Auto Intel",
+            "url": "http://sites:8888/ericfrommelt2015/_site/work/auto-intel/"
           }
         ]
       }
